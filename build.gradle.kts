edbfb26e544ba0d95eebbafcf24e31ad99d73bde
david steinsland <david@davidsteinsland.net>
2024-05-22 10:23:23 +0200

    kotlin 2️⃣.0️⃣.0️⃣

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ group = "com.github.navikt"
 version = properties["version"] ?: "local-build"
 
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.0.0"
     id("java")
     id("maven-publish")
 }
