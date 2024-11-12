c443e8a28bb023c986ec38c18a86ce61758ca75f
david steinsland <david@davidsteinsland.net>
2024-11-12 08:26:33 +0100

    Bump Kotlin JVM -> 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ group = "com.github.navikt"
 version = properties["version"] ?: "local-build"
 
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
     id("java")
     id("maven-publish")
 }
