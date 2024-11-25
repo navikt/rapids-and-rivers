253c42b704487029f80dfad15f979c918b68c63a
david steinsland <david@davidsteinsland.net>
2024-11-25 11:05:40 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val logbackClassicVersion = "1.5.12"
 val logbackEncoderVersion = "8.0"
 val awaitilityVersion = "4.2.2"
 val kafkaTestcontainerVersion = "1.20.4"
-val tbdLibsVersion = "2024.11.25-10.16-d75e0dd1"
+val tbdLibsVersion = "2024.11.25-10.59-6f263a10"
 
 group = "com.github.navikt"
 version = properties["version"] ?: "local-build"
